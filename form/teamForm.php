0b2a74ddefbb40e93881644cb01d309fcd7125eb
scroacher <s.roach@yahoo.com>
2016-07-21 17:10:34 -0400

    Update 1

--- form/teamForm.php
+++ form/teamForm.php
@@ -2,12 +2,13 @@
 <head>
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-	<form id="form1" action="../proc/process1.php" target="second" method="POST">
-   
-	<select name="Name">
-	   <option value="A">Team 1</option>
-	   <option value="B">Team 2</option>
-   </select>
-   <input type="Submit" value="Submit"/>
-</form>
+<div class="forms">
+	<form id="form1" action="playerForm.php" target="second" method="POST">
+			<select name="Name">
+			   <option value="Team 1">Team 1</option>
+			   <option value="Team 2">Team 2</option>
+		   </select>
+		   <input type="Submit" value="Submit"/>
+	</form>
+</div>
 </html>
--- index.php
+++ index.php
@@ -9,24 +9,22 @@
 				<h1>BLeague Keeper Selection</h1>
 			</div>
 		</div>
-		
-		<div class="firstSpace">
-			<iframe src='form/teamForm.php' height="100%">
-				
-			</iframe>
-		</div>
-		
-		<div class="secondSpace">
-			<iframe name="second" height="100%">
-				
-			</iframe>					
-		</div>
-		
-		<div class="thirdSpace">
-			<iframe name="third" height="100%">
-				
-			</iframe>					
-		</div>
-		
+			<div class="formContainer" >
+				<iframe src='form/teamForm.php'>
+
+				</iframe>
+			</div>
+
+			<div class="formContainer">
+				<iframe name="second" height="15%">
+
+				</iframe>					
+			</div>
+
+			<div class="formContainer">
+				<iframe name="third" height="10%">
+
+				</iframe>					
+			</div>
 	</body>
 </html>
